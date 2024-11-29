c4d6456d88b99abc293c869df63bcd108550d197
kevinjosec <125352267+kevinjosec@users.noreply.github.com>
2024-11-29 09:24:08 +0530

    url error

--- src/server/config.js
+++ src/server/config.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const REACT_APP_SERVER_URL="https://17dbcf8f65c0acdbf852ac4e4f38d1ea.serveo.net/api"
-=======
 export const REACT_APP_SERVER_URL=" http://localhost:3000/api"
->>>>>>> 375c8b33043e6273d2a7e1192a6a3a9a7ef60548
